590509b61e87175a391cc316f6c6ac4643cfad34
Yanwu <whzp015258712145@gmail.com>
2026-02-19 15:35:26 +0100

    1
    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/-/', // 仓库名为 -
+  // 如果是在 GitHub Actions 环境下（CI=true），使用仓库名作为基础路径
+  // 否则（如 Vercel 或本地开发）使用根路径 '/'
+  base: process.env.GITHUB_ACTIONS ? '/-/' : '/',
 })
